94c47a73097c0a0718687b525553b940ff29a9ae
newtykins <newtykins@outlook.com>
2022-01-24 20:32:50 +0000

    feat: add github url

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,15 @@
-<script>
+<script lang="ts">
+	// Import required Font Awesome icons
+	import Fa from 'svelte-fa';
+	import { faGithub } from '@fortawesome/free-brands-svg-icons';
+	
 	import '../app.css';
 </script>
 
-<slot />
+<div class="flex items-center justify-center h-screen">
+	<slot />
+
+	<div class="absolute bottom-14">
+		<a href="https://github.com/newtykins/calculator"><Fa icon={faGithub} size="3x" /></a>
+	</div>
+</div>
