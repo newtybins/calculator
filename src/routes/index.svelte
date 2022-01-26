02e4f9165fb8ac090174d579897d11782951f991
newtykins <newtykins@outlook.com>
2022-01-26 19:03:53 +0000

    feat: add keyboard input and validate operators

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,26 @@
 <script lang="ts">
 	import * as math from 'mathjs';
 	import update from 'immutability-helper';
+	import { onMount } from 'svelte';
 
 	let operations: string[] = [];
 	let displayElement: HTMLElement;
 	let answer = ''; // save the answer for quick input
+	const validOperators: string[] = [];
+
+	onMount(() => {
+		const operatorElements = document.getElementsByClassName('op');
+
+		for (let i = 0; i < operatorElements.length; i++) {
+			const operatorElement = operatorElements[i];
+			validOperators.push(operatorElement.textContent);
+		}
+	})
 
 	const handleClick = (e: Event) => {
 		// @ts-ignore this does exist, silly
 		const value = e.target.innerText;
+		const lastOperation = operations[operations.length - 1];
 
 		switch (value) {
 			case 'C':
@@ -25,33 +37,80 @@
 
 			default:
 				// Ensure that two of the same operator can not be touching
-				if (isNaN(value) && operations[operations.length - 1] === value) return;
+				console.log(operations);
+				if (isNaN(parseInt(value)) && validOperators.includes(lastOperation)) return;
+				if (!lastOperation && isNaN(parseInt(value))) return;
 				operations = update(operations, { $push: [value] });
 				break;
 		}
 	}
 
+	const fetchExpression = () => operations.join('').replace(/×/g, '*').replace(/÷/g, '/');
+
 	const calculate = () => {
-		const expression = operations.join('');
+		const expression = fetchExpression();
 
 		try {
 			const output = math.evaluate(expression);
 			answer = math.format(output, { precision: 14 });
 			operations = [answer];
 		} catch (e) {
+			console.error(e);
 			displayElement.style.backgroundColor = '#ff6961';
 			setTimeout(() => {displayElement.style.backgroundColor = null}, 1000);
 		}
 	}
+
+	const onKeyDown = (e: KeyboardEvent) => {
+		const code = e.code;
+		const lastOperation = operations[operations.length - 1];
+		
+		if (e.code.startsWith('Digit') && !e.shiftKey) {
+			const digit = code.split('Digit')[1];
+			operations = update(operations, { $push: [digit] });
+		} else if (!isNaN(parseInt(lastOperation))) {
+			if (e.shiftKey) {
+				switch (e.code) {
+					case 'Digit8':
+						if (lastOperation === '×') return;
+						operations = update(operations, { $push: ['×'] });
+						break;
+
+					case 'Equal':
+						if (lastOperation === '+') return;
+						operations = update(operations, { $push: ['+'] });
+						break;
+				}
+			} else {
+				switch (e.code) {
+					case 'Equal':
+						calculate();
+						break;
+
+					case 'Minus':
+						if (lastOperation === '-') return;
+						operations = update(operations, { $push: ['-'] });
+						break;
+
+					case 'Slash':
+						if (lastOperation === '÷') return;
+						operations = update(operations, { $push: ['÷'] });
+						break;
+				}
+			}
+		}
+	}
 </script>
 
+<svelte:window on:keydown={onKeyDown} />
+
 <div class="grid grid-flow-row auto-rows-max grid-cols-4">
-	<div class="bg-black col-span-4 p-10 text-white text-3xl h-28" bind:this={displayElement}>{operations.join(' ') ?? ''}</div>
+	<div class="bg-black col-span-4 p-10 text-white text-3xl h-28" bind:this={displayElement}>{operations.join('') ?? ''}</div>
 
 	<div class="button" on:click={handleClick}>C</div>
-	<div class="button" on:click={handleClick}>÷</div>
-	<div class="button" on:click={handleClick}>×</div>
-	<div class="button" on:click={handleClick}>-</div>
+	<div class="button op" on:click={handleClick}>÷</div>
+	<div class="button op" on:click={handleClick}>×</div>
+	<div class="button op" on:click={handleClick}>-</div>
 
 	<div class="button" on:click={handleClick}>7</div>
 	<div class="button" on:click={handleClick}>8</div>
@@ -61,7 +120,7 @@
 	<div class="button" on:click={handleClick}>4</div>
 	<div class="button" on:click={handleClick}>5</div>
 	<div class="button" on:click={handleClick}>6</div>
-	<div class="button" on:click={handleClick}>+</div>
+	<div class="button op" on:click={handleClick}>+</div>
 
 	<div class="button" on:click={handleClick}>1</div>
 	<div class="button" on:click={handleClick}>2</div>
